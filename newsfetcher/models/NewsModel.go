e4dd7b0b59372bc089c13522e948257d7991ee70
Tanush Golwala <tanushgolwala@gmail.com>
2025-10-30 22:36:18 +0530

    fix : DB error

--- newsfetcher/models/NewsModel.go
+++ newsfetcher/models/NewsModel.go
@@ -1,13 +1,15 @@
 package models
 
+import "github.com/google/uuid"
+
 type NewsModel struct {
 	BaseModel
-	Title       string  `gorm:"type:varchar(255)" json:"title"`
-	Description string  `gorm:"type:text" json:"description"`
-	Link        string  `gorm:"type:varchar(255);unique" json:"link"`
-	ImageURL    string  `gorm:"type:varchar(255)" json:"image_url"`
-	Author      string  `gorm:"type:varchar(255)" json:"author"`
-	Tags        string  `gorm:"type:varchar(255)" json:"tags"`
-	HashVal     string  `gorm:"type:varchar(255)" json:"hash_val"`
-	Bias        float64 `json:"bias"`
+	Title       string    `gorm:"type:varchar(255)" json:"title"`
+	Description string    `gorm:"type:text" json:"description"`
+	Link        string    `gorm:"type:varchar(255);unique" json:"link"`
+	ImageURL    string    `gorm:"type:varchar(255)" json:"image_url"`
+	Author      string    `gorm:"type:varchar(255)" json:"author"`
+	Tags        string    `gorm:"type:varchar(255)" json:"tags"`
+	HashVal     uuid.UUID `gorm:"type:uuid" json:"hash_val"`
+	Bias        float64   `json:"bias"`
 }
